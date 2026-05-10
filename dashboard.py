8a4892d362925437c284cef264077c500b91ab90
Neeharika-J <neeharikajugran@gmail.com>
2026-05-11 00:11:43 +0530

    ref is not a column in gpd

--- dashboard.py
+++ dashboard.py
@@ -234,13 +234,13 @@ if user_date != "Select":
             st.subheader("Highways around the station")
             highways_m = plot_geo_features_around_station(lat,lon,radius,highway_tags,'yellow')
             st_folium(highways_m, width=700, height=500, key="map_3")
-            highways["identifier"] = (
-                highways["name"]
-                .combine_first(highways["ref"])
-                .fillna("highway segment")
-            )
+            # highways["identifier"] = (
+            #     highways["name"]
+            #     .combine_first(highways["ref"])
+            #     .fillna("highway segment")
+            # )
 
-            highways_nearby = highways['identifier'].dropna().nunique()
+            #highways_nearby = highways['identifier'].dropna().nunique()
             # highways_nearby = highways['name'].nunique()
             
 
